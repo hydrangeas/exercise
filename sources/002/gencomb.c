9f4acc7ef7b2cc47807147594f02e4abe7937b61
Akira Otaka <ootaka@zenkei.com>
2012-12-25 01:26:15 +0900

    add a new function of combination

---
+++ sources/002/gencomb.c
@@ -0,0 +1,50 @@
+/*
+ * Algorithm Dictionary - p60
+ *
+ * refs>
+ * Samuel P. Harbison and Guy L. Steele Jr. C:
+ *   A Reference Manual. Prentice-Hall, second edition 1987. 176pages
+ */
+
+#include "wallet.h"
+
+#define N 8
+#define K 4
+typedef unsigned int set;
+#define first(n) ((set) ((1U << (n)) - 1U))
+
+set nextset(set x)
+{
+  set smallest, ripple, new_smallest, ones;
+
+  smallest = x & -x;
+  ripple = x + smallest;
+  new_smallest = ripple & -ripple;
+  ones = ((new_smallest / smallest) >> 1) - 1;
+  return ripple | ones;
+}
+
+void printset(set s)
+{
+  int i;
+
+  for (i = 1; i <= N; i++) {
+    if (s & 1) printf(" %d", i);
+    s >>= 1;
+  }
+  printf("\n");
+}
+
+int main()
+{
+  int i;
+  set x;
+
+  i = 1; x = first(K);
+  while (! (x & ~first(N))) {
+    printf("%4d:", i); printset(x);
+    x = nextset(x); i++;
+  }
+  return EXIT_SUCCESS;
+}
+
